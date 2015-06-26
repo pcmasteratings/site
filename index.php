a6836991bf735326828ce5a8c219e040ee27cc9b
Jenjen1324 <jens@northcode.no>
2015-06-26 23:27:22 +0200

    Quickfix

--- index.php
+++ index.php
@@ -19,40 +19,46 @@ require("res/include.php");
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
             <div class="col-sm-6 col-md-4">
                 <img src="/tmp/thumbnail.svg" alt="thumbnail">
                 <div class="caption">
-                <h3>Some game</h3>
-                <p> ...... </p>
+                    <h3>Some game</h3>
+                    <p> ...... </p>
+                </div>
             </div>
         </div>
     </div>
