58eef9550f79a362cef172ffaa309aa3838e5e50
Jenjen1324 <jens@northcode.no>
2015-06-26 22:41:41 +0200

    Testing frontpage

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require("res/includes.php");
+require("res/include.php");
 
 ?>
 <!DOCTYPE html>
@@ -14,10 +14,53 @@ require("res/includes.php");
 
 <div class="container">
     <div class="col-md-8">
-        <h1>News</h1>
+        <h1>Top games</h1>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                <div class="caption">
+                <h3>Some game</h3>
+                <p> ...... </p>
+            </div>
+        </div>
     </div>
-    <div class="col-md4">
-        <h1>Some games</h1>
+    <div class="col-md-4">
+        <h1>Other stuff</h1>
+
+        
+
     </div>
 
 </div>
