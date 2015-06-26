2aa41a2b381a3959aad4a536c90490b937b9daff
Andreas Larsen <northcode.no@gmail.com>
2015-06-26 20:03:00 +0200

    moved game.html to game.php and removed the html one

--- game.php
+++ game.php
@@ -1,8 +1,19 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<link href="css/bootstrap.min.css" rel="stylesheet/css" />
+		<link href="/css/bootstrap.min.css" rel="stylesheet" />
+		<meta charset="UTF-8" />
 	</head>
 	<body>
-		<script src="js/bootstrap.js"></script>
+		<div class="container">
+			<h1>GAME TITLE</h1>
+			<div class="col-md-4">
+			</div>
+			<div class="col-md-8">
+				<p>GAME description goes here Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac nulla at arcu egestas convallis eget id odio. Curabitur magna felis, congue quis pretium id, pulvinar sed turpis. Praesent ac tortor tortor. In hac habitasse platea dictumst. Morbi fringilla sapien in lectus ultrices, quis varius sapien condimentum. Vivamus fringilla condimentum risus, nec egestas libero sagittis nec. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. </p>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script src="/js/bootstrap.js"></script>
 	</body>
 </html>
