0baa7c99bbc5d58446fbe023111890c33b5b48bc
Jenjen1324 <jens@northcode.no>
2015-06-26 20:46:32 +0200

    Revert "Revert"
    This reverts commit d064cc00ad85d57e577f3e49e1d3eeb0bcf919d0.

--- res/include.php
+++ res/include.php
@@ -2,4 +2,9 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
+<<<<<<< HEAD
+=======
+
+>>>>>>> ffaf97669c09deff83d66c24a61a467763109496
+// Libaries necessary for every page go in here
 ?>
