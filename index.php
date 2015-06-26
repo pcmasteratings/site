f43556a056a84c28ecf97184d5ee169de8220399
Jenjen1324 <jens@northcode.no>
2015-06-26 20:34:44 +0200

    Stuff

--- index.php
+++ index.php
@@ -6,14 +6,20 @@ require("res/includes.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<?php include("res/head.php"); ?>
-	<title>PC Masterratings</title>
+    <?php include("res/head.php"); ?>
+    <title>PC Masterratings</title>
 </head>
 <body>
 <?php include("res/nav.php"); ?>
 
 <div class="container">
-Stuff goes here
+    <div class="col-md-8">
+        <h1>News</h1>
+    </div>
+    <div class="col-md4">
+        <h1>Some games</h1>
+    </div>
+
 </div>
 
 <?php include("res/footer.php"); ?>
