137f3b3676d741aedacdffe97c61af1b3c50f634
Jenjen1324 <jens@northcode.no>
2015-06-26 19:40:58 +0200

    oops

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require("res/includes.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("res/head.php"); ?>
+	<title>PC Masterratings</title>
+</head>
+<body>
+<?php include("res/nav.php"); ?>
+
+<div class="container">
+Stuff goes here
+</div>
+
+<?php include("res/footer.php"); ?>
+
+</body>
+</html>
