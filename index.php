0a9d7a2534c6741ac09af941474cbeec8dd5a2d5
Jenjen1324 <jens@northcode.no>
2015-06-26 23:35:59 +0200

    Quick fix

--- index.php
+++ index.php
@@ -17,47 +17,59 @@ require("res/include.php");
         <h1>Top games</h1>
         <div class="row">
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
-                <img src="/tmp/thumbnail.svg" alt="thumbnail">
-                <div class="caption">
-                    <h3>Some game</h3>
-                    <p> ...... </p>
+                <div class="thumbnail">
+                    <img src="/tmp/thumbnail.svg" alt="thumbnail">
+                    <div class="caption">
+                        <h3>Some game</h3>
+                        <p> ...... </p>
+                    </div>
                 </div>
             </div>
         </div>
