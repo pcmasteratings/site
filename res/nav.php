d84e16b88614f7d5c80d72681e0852529fcdda3e
Jenjen1324 <jens@northcode.no>
2015-06-27 15:45:48 +0200

    styling

--- res/nav.php
+++ res/nav.php
@@ -2,6 +2,7 @@
 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -10,7 +11,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">     <img src="/img/logo.png" height="40" style="margin-top:-10px;" alt="PCMRatings Glorious!" /></a>
+      <a class="navbar-brand" style="padding: 2px;" href="#"><img src="/img/logo.png" height="100%" alt="PCMRatings Glorious!" /></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -20,4 +21,5 @@
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
+  </div>
 </nav>
