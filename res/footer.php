1c35c5837a39562de0ed4393f2609cfaf09dc9ca
Jenjen1324 <jens@northcode.no>
2015-07-13 18:47:02 +0200

    Uniforming head/footer

--- res/footer.php
+++ res/footer.php
@@ -2,3 +2,8 @@
 ?>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover();
+    });
+</script>
--- res/head.php
+++ res/head.php
@@ -1,14 +1,8 @@
 <?php
 // Meta tags and stuff
+// NOTE: PUT ALL JS IN FOOTER FOR QUICKER PAGE LOADING
 ?>
 <link rel="icon" type="image/png" href="/img/badges/r_tiny.jpg">
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/bootstrap.min.css">
-<script type="text/javascript" src="js/Jquery-1.4.2-min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('[data-toggle="popover"]').popover();
-    });
-</script>
