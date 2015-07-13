09ea2e868dc125ffb857a4f6bb280a0499051f31
Jenjen1324 <jens@northcode.no>
2015-07-13 18:36:37 +0200

    Fix

--- res/head.php
+++ res/head.php
@@ -5,8 +5,8 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/bootstrap.min.css">
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/Jquery-1.4.2-min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('[data-toggle="popover"]').popover();
